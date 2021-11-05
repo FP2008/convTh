85d545dcb155438bedce96fba5744e697c50c55a
François Poirrier <46083896+FP2008@users.noreply.github.com>
2021-11-05 15:15:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 document.getElementById("Th").addEventListener("input",convTh);
+document.getElementById("dc").addEventListener("input",convTh);
+document.getElementById("df").addEventListener("input",convTh);
 
 function convTh(e){
     //définir id comme une constante non modifiable
   //e = event de l'input
   const id = e.target.id;
   const valeur = parseFloat(e.target.value);
-  dc.value = valeur * 30;
-  df.value = valeur * 30 * 1.8 + 32
- }
+  if(valeur === NaN)return;
+  if(id == "Th"){
+    dc.value = valeur * 30;
+    df.value = valeur * 30 * 1.8 + 32
+  }
+  else if(id == "dc"){
+    Th.value = valeur / 30;
+    df.value = valeur * 1.8 + 32;
+  }
+  else if(id == "df"){
+    Th.value = (valeur - 32 ) / 1.8 / 30;
+    dc.value = (valeur - 32 ) /1.8;
+  }
+}
