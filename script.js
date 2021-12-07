b66f9e38bcdc7b04f7a38ed21b882bd437e243f5
François Poirrier <46083896+FP2008@users.noreply.github.com>
2021-12-07 22:09:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
+/* f.poirrier */
 document.getElementById("Th").addEventListener("input",convTh);
 document.getElementById("dc").addEventListener("input",convTh);
 document.getElementById("df").addEventListener("input",convTh);
+document.getElementById("raz").addEventListener("click",convTh);
+
+var id;
+var valeur;
+
+function testValue(value) {
+  if (isNaN(value) == true){
+    id = "raz";
+  }
+}
 
 function convTh(e){
-    //définir id comme une constante non modifiable
+  //définir id comme une constante non modifiable
   //e = event de l'input
-  const id = e.target.id;
-  const valeur = parseFloat(e.target.value);
-  if(valeur === NaN)return;
+  id = e.target.id;
+  valeur = e.target.value;
+  testValue(valeur);
+  
   if(id == "Th"){
     dc.value = valeur * 30;
     df.value = valeur * 30 * 1.8 + 32
-  }
-  else if(id == "dc"){
+  } else if(id == "dc"){
     Th.value = valeur / 30;
     df.value = valeur * 1.8 + 32;
-  }
-  else if(id == "df"){
-    Th.value = (valeur - 32 ) / 1.8 / 30;
-    dc.value = (valeur - 32 ) /1.8;
-  }
+    }
+    else if(id == "df"){
+      Th.value = (valeur - 32 ) / 1.8 / 30;
+      dc.value = (valeur - 32 ) /1.8;
+    }
+    else if(id == "raz") {
+      document.getElementById("myForm").reset();
+      valeur = "";
+    }
 }
